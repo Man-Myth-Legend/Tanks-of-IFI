acde96ae624727488fcb9727cdf1447a22241649
Noor <noorothma9@gmail.com>
2023-01-21 12:08:07 +0100

    Working on angles

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,17 @@ public class Tank : MonoBehaviour
     public float movementSpeed;
     public float rotateSpeed;
 
+    public int rotateBarrelThreshold;
+    private int barrelHeading;
+    private float tempBarrelHeading;
+
     public GameObject barrel;
+    private UI ui;
     // Start is called before the first frame update
     void Start()
     {
-        
+        barrelHeading = (int) barrel.transform.rotation.eulerAngles.x;
+        ui = GameObject.FindGameObjectWithTag("UI").GetComponent<UI>();
     }
 
     // Update is called once per frame
@@ -22,22 +29,46 @@ public class Tank : MonoBehaviour
     {
         if (activeTank)
         {
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.Translate(new Vector3(0, 0, -movementSpeed));
+            Move();
+            Rotate();
+        }
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(0, 0, -movementSpeed));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(0, 0, movementSpeed));
+        }
+    }
+    void Rotate()
+    {
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tempBarrelHeading -= 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tempBarrelHeading += 1;
+        }
+        if (tempBarrelHeading == rotateBarrelThreshold || tempBarrelHeading == -rotateBarrelThreshold)
+        {
+            if (tempBarrelHeading == rotateBarrelThreshold) {
+                barrel.transform.eulerAngles = new Vector3((int)barrel.transform.eulerAngles.x + 1, 0, 0);
+                barrel.transform.Rotate(new Vector3(1, 0, 0));
+                tempBarrelHeading = 0;
             }
-            else if (Input.GetKey(KeyCode.RightArrow))
+            else
             {
-                transform.Translate(new Vector3(0, 0, movementSpeed));
+                barrel.transform.eulerAngles = new Vector3((int)barrel.transform.eulerAngles.x - 1, 0, 0);
+                tempBarrelHeading = 0;
             }
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                barrel.transform.Rotate(new Vector3(rotateSpeed, 0, 0));
-            }
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                barrel.transform.Rotate(new Vector3(-rotateSpeed, 0, 0));
-            }
+           ui.UpdateAngle((int)barrel.transform.eulerAngles.x);
         }
     }
 }
